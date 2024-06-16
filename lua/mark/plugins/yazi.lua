b4282df9e267e685e4e1174f5bfb2e34f6aea91b
mark <markchristianlacap@gmail.com>
2024-06-16 09:48:07 +0800

    feat: add yazi

---
+++ lua/mark/plugins/yazi.lua
@@ -0,0 +1,22 @@
+---@type LazySpec
+return {
+  "mikavilpas/yazi.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  event = "VeryLazy",
+  keys = {
+    {
+      -- Open in the current working directory
+      "<leader>ee",
+      function()
+        require("yazi").yazi(nil, vim.fn.getcwd())
+      end,
+      desc = "Open the file manager in nvim's working directory",
+    },
+  },
+  ---@type YaziConfig
+  opts = {
+    open_for_directories = false,
+  },
+}
