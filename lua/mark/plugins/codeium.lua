f288c8dc52e1a6573086b26eec2961c4d2f83283
mark <markchristianlacap@gmail.com>
2024-06-19 21:02:54 +0800

    fixed: codeium accept

--- lua/mark/plugins/codeium.lua
+++ lua/mark/plugins/codeium.lua
@@ -3,6 +3,7 @@ return {
   commit = '289eb724e5d6fab2263e94a1ad6e54afebefafb2',
   event = 'BufEnter',
   config = function()
+    vim.g.codeium_disable_bindings = 1
     -- Change '<C-g>' here to any keycode you like.
     vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end, {
       expr = true,
