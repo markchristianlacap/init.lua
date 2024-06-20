e63b7d22266726b640232642a350bc17f6ad203b
Mark Christian Lacap <markchristianlacap@gmail.com>
2024-06-20 10:56:19 +0800

    chore: update

--- lua/mark/plugins/telescope.lua
+++ lua/mark/plugins/telescope.lua
@@ -6,7 +6,8 @@ return {
   config = function()
     local actions = require 'telescope.actions'
     local transform_mod = require('telescope.actions.mt').transform_mod
-
+    local telescope = require 'telescope'
+    local builtin = require 'telescope.builtin'
     local trouble = require 'trouble'
     local trouble_telescope = require 'trouble.sources.telescope'
 
@@ -14,7 +15,7 @@ return {
     local custom_actions = transform_mod {
       open_trouble_qflist = function(prompt_bufnr) trouble.toggle 'quickfix' end,
     }
-    require('telescope').setup {
+    telescope.setup {
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
@@ -24,24 +25,26 @@ return {
         mappings = {
           i = {
             ['<C-k>'] = actions.move_selection_previous, -- move to prev result
-            ['<C-j>'] = actions.move_selection_next, -- move to next result
+            ['<C-j>'] = actions.move_selection_next,     -- move to next result
             ['<C-q>'] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
             ['<C-t>'] = trouble_telescope.open,
           },
         },
       },
+      pickers = {
+        find_files = {
+          theme = 'dropdown',
+        },
+      },
     }
-    local builtin = require 'telescope.builtin'
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find files' })
     vim.keymap.set(
       'n',
-      '<leader>fF',
-      function() require('telescope.builtin').find_files { hidden = true, no_ignore = true } end,
+      '<leader>ff',
+      function() builtin.find_files { hidden = true, no_ignore = true } end,
       { desc = 'Find files (with hidden and no ignore)' }
     )
 
-    vim.keymap.set('n', '<leader>fw', builtin.live_grep, { desc = 'Live grep (with hidden)' })
-    vim.keymap.set('n', '<leader>fW', function()
+    vim.keymap.set('n', '<leader>fw', function()
       builtin.live_grep {
         additional_args = function(args) return vim.list_extend(args, { '--hidden', '--no-ignore' }) end,
       }
