07219ea35613be56044b0fc29a9c7263966a73d1
mark <markchristianlacap@gmail.com>
2024-06-22 10:21:38 +0800

    feat: changed color config

--- lua/mark/keymaps.lua
+++ lua/mark/keymaps.lua
@@ -44,32 +44,29 @@ vim.keymap.set('n', '<leader>q', ':bd<CR>', opts)
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('UserLspConfig', {}),
   callback = function(event)
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
     local opts = { buffer = event.buf, silent = true }
+
     -- set keybinds
     opts.desc = 'Show LSP references'
-    vim.keymap.set('n', 'gR', '<cmd>Telescope lsp_references<CR>', opts) -- show definition, references
+    vim.keymap.set('n', 'gR', vim.lsp.buf.references, opts) -- show definition, references
 
     opts.desc = 'Go to declaration'
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts) -- go to declaration
 
     opts.desc = 'Show LSP definitions'
-    vim.keymap.set('n', 'gd', '<cmd>Telescope lsp_definitions<CR>', opts) -- show lsp definitions
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts) -- show lsp definitions
 
     opts.desc = 'Show LSP implementations'
-    vim.keymap.set('n', 'gi', '<cmd>Telescope lsp_implementations<CR>', opts) -- show lsp implementations
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts) -- show lsp implementations
 
     opts.desc = 'Show LSP type definitions'
-    vim.keymap.set('n', 'gt', '<cmd>Telescope lsp_type_definitions<CR>', opts) -- show lsp type definitions
+    vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, opts) -- show lsp type definitions
 
     opts.desc = 'See available code actions'
     vim.keymap.set({ 'n', 'v' }, '<leader>la', vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
 
     opts.desc = 'Smart rename'
     vim.keymap.set('n', '<leader>lr', vim.lsp.buf.rename, opts) -- smart rename
-
-    opts.desc = 'Show buffer diagnostics'
-    vim.keymap.set('n', '<leader>D', '<cmd>Telescope diagnostics bufnr=0<CR>', opts) -- show  diagnostics for file
 
     opts.desc = 'Show line diagnostics'
     vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, opts) -- show diagnostics for line
--- lua/mark/plugins/lspconfig.lua
+++ lua/mark/plugins/lspconfig.lua
@@ -5,32 +5,25 @@ return {
     'hrsh7th/cmp-nvim-lsp',
     { 'antosha417/nvim-lsp-file-operations', config = true },
     'seblj/roslyn.nvim',
-    'mrshmllow/document-color.nvim',
+    'brenoprata10/nvim-highlight-colors',
   },
   config = function()
     local lspconfig = require 'lspconfig'
     local mason_lspconfig = require 'mason-lspconfig'
     local cmp_nvim_lsp = require 'cmp_nvim_lsp'
-    local document_color = require 'document-color'
-    document_color.setup {}
+    local highlight_color = require 'nvim-highlight-colors'
+    highlight_color.setup {}
+
     local capabilities = cmp_nvim_lsp.default_capabilities()
-    local on_attach = function(client, bufnr)
-      if client.server_capabilities.colorProvider then document_color.buf_attach(bufnr) end
-    end
-    capabilities.textDocument.colorProvider = {
-      dynamicRegistration = true,
-    }
     mason_lspconfig.setup_handlers { -- default handler for installed servers
       function(server_name)
         lspconfig[server_name].setup {
           capabilities = capabilities,
-          on_attach = on_attach,
         }
       end,
       ['volar'] = function()
         lspconfig.volar.setup {
           capabilities = capabilities,
-          on_attach = on_attach,
           init_options = {
             vue = {
               hybridMode = false,
@@ -41,7 +34,6 @@ return {
       ['vtsls'] = function()
         lspconfig.vtsls.setup {
           capabilities = capabilities,
-          on_attach = on_attach,
           settings = {
             vtsls = {
               tsserver = {
@@ -61,11 +53,10 @@ return {
         }
       end,
     }
-    require('roslyn').setup { capabilities = capabilities, on_attach = on_attach }
+    require('roslyn').setup { capabilities = capabilities }
     -- manual lsp config
     require('lspconfig').unocss.setup {
       capabilities = capabilities,
-      on_attach = on_attach,
     }
   end,
 }
