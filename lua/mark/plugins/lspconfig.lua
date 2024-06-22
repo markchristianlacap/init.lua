fb76685e82d69c097c053ba81028245f8666962d
mark <markchristianlacap@gmail.com>
2024-06-22 10:00:40 +0800

    feat: added document color on lsp

--- lua/mark/plugins/lspconfig.lua
+++ lua/mark/plugins/lspconfig.lua
@@ -5,27 +5,32 @@ return {
     'hrsh7th/cmp-nvim-lsp',
     { 'antosha417/nvim-lsp-file-operations', config = true },
     'seblj/roslyn.nvim',
+    'mrshmllow/document-color.nvim',
   },
   config = function()
-    -- import lspconfig plugin
     local lspconfig = require 'lspconfig'
-
-    -- import mason_lspconfig plugin
     local mason_lspconfig = require 'mason-lspconfig'
-
-    -- import cmp-nvim-lsp plugin
     local cmp_nvim_lsp = require 'cmp_nvim_lsp'
-    -- used to enable autocompletion (assign to every lsp server config)
+    local document_color = require 'document-color'
+    document_color.setup {}
     local capabilities = cmp_nvim_lsp.default_capabilities()
+    local on_attach = function(client, bufnr)
+      if client.server_capabilities.colorProvider then document_color.buf_attach(bufnr) end
+    end
+    capabilities.textDocument.colorProvider = {
+      dynamicRegistration = true,
+    }
     mason_lspconfig.setup_handlers { -- default handler for installed servers
       function(server_name)
         lspconfig[server_name].setup {
           capabilities = capabilities,
+          on_attach = on_attach,
         }
       end,
       ['volar'] = function()
         lspconfig.volar.setup {
           capabilities = capabilities,
+          on_attach = on_attach,
           init_options = {
             vue = {
               hybridMode = false,
@@ -36,6 +41,7 @@ return {
       ['vtsls'] = function()
         lspconfig.vtsls.setup {
           capabilities = capabilities,
+          on_attach = on_attach,
           settings = {
             vtsls = {
               tsserver = {
@@ -55,10 +61,11 @@ return {
         }
       end,
     }
-    require('roslyn').setup { capabilities = capabilities }
+    require('roslyn').setup { capabilities = capabilities, on_attach = on_attach }
     -- manual lsp config
     require('lspconfig').unocss.setup {
       capabilities = capabilities,
+      on_attach = on_attach,
     }
   end,
 }
