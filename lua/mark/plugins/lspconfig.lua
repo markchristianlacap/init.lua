5ac77ae08d1d37cfd5bfc56f75bbe0a7732ce827
Mark Christian Lacap <markchristianlacap@gmail.com>
2024-06-28 10:48:36 +0800

    chore: removed lspkind

--- lua/mark/plugins/lspconfig.lua
+++ lua/mark/plugins/lspconfig.lua
@@ -41,7 +41,7 @@ return {
                   {
                     name = '@vue/typescript-plugin',
                     location = require('mason-registry').get_package('vue-language-server'):get_install_path()
-                        .. '/node_modules/@vue/language-server',
+                      .. '/node_modules/@vue/language-server',
                     languages = { 'vue' },
                     configNamespace = 'typescript',
                     enableForWorkspaceTypeScriptVersions = true,
