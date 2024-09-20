f0bf3f1d91a8486dd51f57a968602a10201f59ad
Mark Christian Lacap <markchristianlacap@gmail.com>
2024-09-20 11:09:50 +0800

    chore: add yank highlight

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -18,3 +18,11 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     end
   end,
 })
+
+vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = 'YankHighlight',
+  callback = function()
+    vim.highlight.on_yank { higroup = 'IncSearch', timeout = 200 }
+  end,
+})
