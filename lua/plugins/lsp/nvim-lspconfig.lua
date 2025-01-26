28036748c3d2fe3d2ce9935ead94c744ec39ae3a
mark <markchristianlacap@gmail.com>
2025-01-26 22:48:12 +0800

    chore: update deps

--- lua/plugins/lsp/nvim-lspconfig.lua
+++ lua/plugins/lsp/nvim-lspconfig.lua
@@ -2,24 +2,16 @@ if vim.g.vscode then return {} end
 return {
   "neovim/nvim-lspconfig",
   event = { "BufReadPre", "BufNewFile" },
-  dependencies = {
-    "hrsh7th/cmp-nvim-lsp",
-  },
   config = function()
     local lspconfig = require "lspconfig"
     local mason_lspconfig = require "mason-lspconfig"
-    local cmp_nvim_lsp = require "cmp_nvim_lsp"
 
-    local capabilities = cmp_nvim_lsp.default_capabilities()
     mason_lspconfig.setup_handlers { -- default handler for installed servers
       function(server_name)
-        lspconfig[server_name].setup {
-          capabilities = capabilities,
-        }
+        lspconfig[server_name].setup {}
       end,
       ["volar"] = function()
         lspconfig.volar.setup {
-          capabilities = capabilities,
           settings = {
             vue = {
               complete = {
@@ -29,12 +21,18 @@ return {
                 }
               }
             }
+          },
+          init_options = {
+            typescript = {
+              tsdk = vim.fn.expand(
+                "~/.local/share/nvim/mason/packages/vue-language-server/node_modules/typescript/lib"
+              ),
+            },
           }
         }
       end,
       ["vtsls"] = function()
         lspconfig.vtsls.setup {
-          capabilities = capabilities,
           filetypes = { "typescript", "javascript", "vue" },
           settings = {
             vtsls = {
