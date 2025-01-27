06e254ab8ee7e1adb7a79d5f217e77d112a59e63
mark <markchristianlacap@gmail.com>
2025-01-27 22:05:28 +0800

    fixed: format in lsp

--- lua/plugins/lsp/nvim-lspconfig.lua
+++ lua/plugins/lsp/nvim-lspconfig.lua
@@ -1,13 +1,18 @@
 if vim.g.vscode then return {} end
 return {
   "neovim/nvim-lspconfig",
-    dependencies = { 'saghen/blink.cmp' },
+  dependencies = { "saghen/blink.cmp" },
   event = { "BufReadPre", "BufNewFile" },
   config = function()
     local lspconfig = require "lspconfig"
     local mason_lspconfig = require "mason-lspconfig"
- local capabilities = require('blink.cmp').get_lsp_capabilities()
+    local capabilities = require('blink.cmp').get_lsp_capabilities()
     mason_lspconfig.setup_handlers { -- default handler for installed servers
+      function(server_name)
+        lspconfig[server_name].setup {
+          capabilities = capabilities,
+        }
+      end,
       ["volar"] = function()
         lspconfig.volar.setup {
           capabilities = capabilities,
