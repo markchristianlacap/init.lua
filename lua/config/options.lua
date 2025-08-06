9cff3722e270c996cd6b29a9846517788a4631c7
Mark Christian Lacap <markchristianlacap@gmail.com>
2025-08-06 18:45:09 +0800

    feat: update conf

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,20 +1,43 @@
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.winborder = "rounded"
-vim.opt.tabstop = 4
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.wrap = false
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
-vim.opt.termguicolors = true
-vim.opt.signcolumn = "yes"
-vim.opt.smartcase = true
-vim.opt.ignorecase = true
-vim.opt.colorcolumn = "80"
+local opt = vim.opt
 
+opt.relativenumber = true
+opt.number = true
+
+-- tabs & indentation
+opt.tabstop = 2       -- 2 spaces for tabs (prettier default)
+opt.shiftwidth = 2    -- 2 spaces for indent width
+opt.expandtab = true  -- expand tab to spaces
+opt.autoindent = true -- copy indent from current line when starting new one
+
+opt.wrap = false
+
+-- search settings
+opt.ignorecase = true -- ignore case when searching
+opt.smartcase = true  -- if you include mixed case in your search, assumes you want case-sensitive
+
+opt.cursorline = true
+
+-- turn on termguicolors for tokyonight colorscheme to work
+-- (have to use iterm2 or any other true color terminal)
+opt.termguicolors = true
+opt.background = "dark" -- colorschemes that can be light or dark will be made dark
+opt.signcolumn = "yes"  -- show sign column so that text doesn't shift
+
+-- backspace
+opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+
+-- split windows
+opt.splitright = true -- split vertical window to the right
+opt.splitbelow = true -- split horizontal window to the bottom
+
+-- turn off swapfile
+opt.swapfile = false
+
+-- enable mouse mode
+vim.o.mouse = 'a'
+
+
+vim.g.have_nerd_font = true
 vim.diagnostic.config {
   signs = {
     text = {
