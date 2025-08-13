db757cdd4b10b6358ca186b5f689e251a5446e42
mark <markchristianlacap@gmail.com>
2025-08-13 22:28:29 +0800

    chore: add comment string

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,5 +1,19 @@
 if vim.g.vscode then return {} end
 return {
   "echasnovski/mini.comment",
-  opts = {},
+  dependencies = {
+    'JoosepAlviste/nvim-ts-context-commentstring',
+  },
+  config = function()
+    require('ts_context_commentstring').setup {
+      enable_autocmd = false,
+    }
+    require('mini.comment').setup {
+      options = {
+        custom_commentstring = function()
+          return require('ts_context_commentstring').calculate_commentstring() or vim.bo.commentstring
+        end,
+      },
+    }
+  end,
 }
